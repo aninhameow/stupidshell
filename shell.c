71f11ee1ccedca7d5da2fccc1f814a879bda99e0
Ana <anaprovavelmenteidk@proton.me>
2026-02-13 21:36:54 +0000

    Add basic built ins (cd and exit)

--- shell.c
+++ shell.c
@@ -38,14 +38,33 @@ int main() {
     char *args[64];
     char input[128];
     while (1) {
+    printf("> ");
     fgets(input, sizeof(input), stdin);
     parse_input(input, args);
+    
+    if (strcmp(args[0], "exit") == 0) {
+        break;
+    } else if (strcmp(args[0], "cd") == 0) {
+        if (args[2] != NULL)
+        {
+            fprintf(stderr, "cd: too many arguments\n");
+        } else if (args[1] == NULL) {
+            fprintf(stderr, "cd: missing argument\n");
+        } else {
+            if (chdir(args[1]) != 0) {
+                perror("cd failed");
+            }
+            chdir(args[1]);
+        }
+        
+    } else {
         pid_t pid = fork();
         if (pid == 0) {
             exec_process(args[0], args);
         } else {
             waitpid(pid, NULL, 0);
         }
+    }
 }
 
     return 0;
